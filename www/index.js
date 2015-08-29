cd8138413a50f3f4735816862b79c78890559e92
Josh Terrell <joshterrell805@gmail.com>
2015-08-28 17:20:40 -0700

    scroll chart automajically

--- www/index.js
+++ www/index.js
@@ -5,30 +5,53 @@ require.config({
     // plugins
     text: 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
     json: 'https://cdnjs.cloudflare.com/ajax/libs/requirejs-plugins/1.0.3/json.min',
+    lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.min',
   }
 });
 
-require(['d3', 'c3', 'json!CJES.json'], function(d3, c3, rawData) {
-  var data = {
-    high: rawData.indicators.quote[0].high.slice(-30),
-    low: rawData.indicators.quote[0].low.slice(-30),
-  };
-  data.high.unshift('high');
-  data.low.unshift('low');
+require(['d3', 'c3', 'lodash', 'json!CJES.json'], function(d3, c3, _, rawData) {
+  rawData.timestamp = _.map(rawData.timestamp, function(ts) {return ts*1000;});
+  var index = 0;
+  var count = 30;
+
+  function columnsFrom(data, index, count) {
+    return [
+      ['high'].concat(
+          data.indicators.quote[0].high.slice(index, index + count)),
+      ['low'].concat(
+          data.indicators.quote[0].low.slice(index, index + count)),
+      ['x'].concat(data.timestamp.slice(index, index + count)),
+    ];
+  }
+
+  function chartData(data, beginIndex, count) {
+    return {
+      columns: columnsFrom(data, beginIndex, count),
+      x: 'x',
+    };
+  }
+
+  function nextChartData() {
+    return chartData(rawData, index++, count);
+  }
 
   var chart = c3.generate({
     bindto: '#chart',
-    data: {
-      columns: [
-        data.high,
-        data.low,
-      ],
-      /*
-      types: {
-        'high': 'line',
-        'low': 'line',
+    data: nextChartData(),
+    transition: {
+      duration: 0,
+    },
+    axis: {
+      x: {
+        type: 'timeseries',
+        tick: {
+          format: '%Y-%m-%d',
+        },
       },
-      */
     },
   });
+
+  setInterval(function() {
+    chart.load(nextChartData());
+  }, 1000)
 });
