5cc27cc041ea59b4267fc3e8f53f91a3ee5a4c29
Josh Terrell <joshterrell805@gmail.com>
2015-08-30 07:38:08 -0700

    neural net with better features

--- train/train.R
+++ train/train.R
@@ -3,6 +3,7 @@ library(neuralnet)
 
 
 quotes         = read.csv('data/CJES.csv');
+quotes         = na.omit(quotes);
 tradingIndexes = read.csv('data/CJES_tradingIndexes.csv');
 buyTimestamps  = quotes[tradingIndexes[,'buy_index'], 'timestamp'];
 
@@ -10,50 +11,45 @@ quotes$should_buy = as.integer(quotes$timestamp %in% buyTimestamps);
 quotes$price      = rowMeans(subset(quotes, select=c(high, low)));
 quotes$date       = as.Date(as.POSIXct(quotes$timestamp, origin="1970-01-01"));
 
-quotes$price_prev = sapply(1:nrow(quotes), function(i) {
-  if (i < 2) {
-    return(NA);
-  } else {
-    return(quotes[i-1, 'price']);
+# vma and sma
+# and vma[p1] - vma[p2]
+periods = c(5, 15, 45, 90, 180);
+column_names = c();
+
+for (period in periods) {
+  column_names = c(column_names, paste('sma', period, sep='_'),
+      paste('vma', period, sep='_'));
+  quotes[,paste('sma', period, sep='_')] = sma(quotes$price, period);
+  quotes[,paste('vma', period, sep='_')] = sma(quotes$volume, period);
+}
+
+for (i in 1:length(periods)) {
+  if (i == length(periods)) {
+    break;
   }
-});
+  for (j in (i+1):length(periods)) {
+    column_names = c(column_names,
+        paste('sma', periods[i], periods[j], sep='_'),
+        paste('vma', periods[i], periods[j], sep='_'));
+    quotes[,paste('sma', periods[i], periods[j], sep='_')] =
+        quotes[,paste('sma', periods[i], sep='_')] -
+        quotes[,paste('sma', periods[j], sep='_')];
+    quotes[,paste('vma', periods[i], periods[j], sep='_')] =
+        quotes[,paste('vma', periods[i], sep='_')] -
+        quotes[,paste('vma', periods[j], sep='_')];
+  }
+}
+quotes = na.omit(quotes);
 
-# print(quotes[900:920,]);
-
-## vma and sma
-## and vma[p1] - vma[p2]
-#periods = c(5, 15, 45, 90, 180);
-#
-#for (period in periods) {
-#  quotes[,paste('sma', period)] = sma(quotes$price, period);
-#  quotes[,paste('vma', period)] = sma(quotes$volume, period);
-#}
-#
-#for (i in 1:length(periods)) {
-#  if (i == length(periods)) {
-#    break;
-#  }
-#  for (j in (i+1):length(periods)) {
-#    quotes[,paste('sma', periods[i], periods[j])] =
-#        quotes[,paste('sma', periods[i])] - quotes[paste('sma', periods[j])];
-#    quotes[,paste('vma', periods[i], periods[j])] =
-#        quotes[,paste('vma', periods[i])] - quotes[paste('vma', periods[j])];
-#  }
-#}
-#print(quotes[180:185,]);
-
-quotes = quotes[!is.na(quotes$price),];
-quotes = quotes[!is.na(quotes$price_prev),];
-
-net <- neuralnet(should_buy~price+price_prev, quotes);
+f = as.formula(paste('should_buy ~ ', paste(column_names, collapse='+'))); 
+layers = c(length(column_names), length(column_names), length(column_names));
+net = neuralnet(f, hidden=layers, quotes);
 print(net);
 # plot(net);
-test = quotes[,c('price', 'price_prev')];
-# print(test);
+test = quotes[,column_names];
 results = compute(net, test);
-#print(results$net.result);
 
-cleanoutput <- cbind(quotes$price, quotes$price_prev, quotes$should_buy,
+cleanoutput = cbind(quotes$date, quotes$should_buy,
     as.data.frame(results$net.result));
-colnames(cleanoutput) <- c('price','price_prev','should_buy','predicted');
+colnames(cleanoutput) = c('date', 'should_buy', 'predicted');
 print(cleanoutput);
