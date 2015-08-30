98969a18239c082327a0267e53e03cd1b888b6b1
Josh Terrell <joshterrell805@gmail.com>
2015-08-29 17:42:53 -0700

    graph trade regions

--- graph/graph.R
+++ graph/graph.R
@@ -1,19 +1,26 @@
 library('ggplot2');
 
-data = read.csv('data/CJES.csv');
-data = data[100:130,];
+doIt = function() {
+  quotes         = read.csv('data/CJES.csv');
+  tradingIndexes = read.csv('data/CJES_tradingIndexes.csv');
+  quotes = quotes[tradingIndexes[3,1]:tradingIndexes[3,2],];
 
-data$date = as.Date(as.POSIXct(data$timestamp, origin="1970-01-01"))
-data$price = rowMeans(subset(data, select=c(high, low)));
+  quotes$date = as.Date(as.POSIXct(quotes$timestamp, origin="1970-01-01"))
+  quotes$price = rowMeans(subset(quotes, select=c(high, low)));
 
-graph <- ggplot() +
-    geom_line(data=data, aes(x=date, y=high, colour='high')) + 
-    geom_line(data=data, aes(x=date, y=price, colour='price')) + 
-    geom_line(data=data, aes(x=date, y=low, colour='low')) +
-    scale_colour_manual(
-        values=c('red', 'black', 'gray'),
-        breaks=c('high', 'low', 'price')
-    ) +
-    labs(colour='Legend');
+  graph <- ggplot() +
+      geom_line(data=quotes, aes(x=date, y=high, colour='high')) + 
+      geom_line(data=quotes, aes(x=date, y=price, colour='price')) + 
+      geom_line(data=quotes, aes(x=date, y=low, colour='low')) +
+      scale_colour_manual(
+          values=c('red', 'black', 'gray'),
+          breaks=c('high', 'low', 'price')
+      ) +
+      labs(colour='Legend') +
+      xlab('date') +
+      ylab('price');
 
-print(graph);
+  print(graph);
+}
+
+doIt();
