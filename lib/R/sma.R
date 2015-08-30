b9174b4702f42ff69336f31ef12768cb05375b78
Josh Terrell <joshterrell805@gmail.com>
2015-08-30 10:33:03 -0700

    better prediction 18->15%

--- lib/R/sma.R
+++ lib/R/sma.R
@@ -1,13 +1,12 @@
 # calculate simple moving average of data
 # assumes data is at least 'count' long
-sma = function(data, count) {
-  return(sapply(1:length(data), function(i) {
+sma = function(data, column, count) {
+  seconds_per_day = 24*60*60;
+  return(sapply(1:nrow(data), function(i) {
     if (i < count) {
       return(NA);
     } else {
-      return(mean(
-        data[(i-count+1):i]
-      ));
+      return(sum(data[(i-count+1):i, column]) / count);
     }
   }));
 };
