ce330ff3b3ef4adab17039cbe6308af58d9afe7c
Josh Terrell <joshterrell805@gmail.com>
2015-08-30 18:35:01 -0700

    plot graphs of each trade point
    2/3rds before is input, 1/3 after is sell range

--- graph/graph.R
+++ graph/graph.R
@@ -1,33 +1,75 @@
 library('ggplot2');
+source('lib/findBuyIndexes.R');
+source('lib/sma.R');
 
 quotes         = read.csv('data/CJES.csv');
-tradingIndexes = read.csv('data/CJES_tradingIndexes.csv');
+quotes         = na.omit(quotes);
+buyIndexes     = findBuyIndexes(quotes, maxHoldDays=30, minSellToBuyRatio=1.2);
+buyIndexes     = buyIndexes[buyIndexes > 60];
+print(buyIndexes);
+print(length(buyIndexes));
+buyIndex = buyIndexes[7];
 
-quotes$date    = as.Date(as.POSIXct(quotes$timestamp, origin="1970-01-01"))
-quotes$price   = rowMeans(subset(quotes, select=c(high, low)));
-quotes$sma_005  = sma(quotes$price, 5);
-quotes$sma_015  = sma(quotes$price, 15);
-quotes$sma_045  = sma(quotes$price, 45);
-quotes$sma_090  = sma(quotes$price, 90);
+quotes$date     = as.Date(as.POSIXct(quotes$timestamp, origin="1970-01-01"))
+quotes$price    = rowMeans(subset(quotes, select=c(high, low)));
+quotes$sma_015  = sma(quotes, 'price', 15);
+quotes$sma_030  = sma(quotes, 'price', 30);
+quotes$sma_060  = sma(quotes, 'price', 60);
+quotes$vma_060  = sma(quotes, 'volume', 60);
+quotes          = na.omit(quotes);
+growth_field = 'sma_015';
+quotes$price_growth = sapply(1:nrow(quotes), function(i) {
+  if (i == 1) {
+    return(NA);
+  } else {
+    q = quotes[i,];
+    this = q[,growth_field];
+    last = tail(quotes[quotes$timestamp < q$timestamp, growth_field], 1);
+    (this - last) / last * 100;
+  }
+});
+growth_field = 'vma_060';
+quotes$volume_growth = sapply(1:nrow(quotes), function(i) {
+  if (i == 1) {
+    return(NA);
+  } else {
+    q = quotes[i,];
+    this = q[,growth_field];
+    last = tail(quotes[quotes$timestamp < q$timestamp, growth_field], 1);
+    (this - last) / last * 100;
+  }
+});
+quotes = quotes[2:nrow(quotes), ];
 
-quotes = quotes[(tradingIndexes[3,1]-90):tradingIndexes[3,2],];
+quotes = quotes[(buyIndex-60):(buyIndex+30),];
 
+growthSpan = max(quotes$price_growth) - min(quotes$price_growth);
+ylimits = c(floor(min(quotes$price) - growthSpan), ceiling(max(quotes$price)));
+quotes$price_growth = quotes$price_growth + ylimits[1] + growthSpan/2;
+quotes$volume_growth = quotes$volume_growth + ylimits[1] + growthSpan/2;
+
+fn_vol <- smooth.spline(x=quotes$timestamp, y=quotes$volume_growth);
+quotes$volume_growth_smooth = data.frame(predict(fn_vol, quotes$timestamp))$y;
 
 graph <- ggplot() +
-    geom_line(data=quotes, aes(x=date, y=price, colour='price')) + 
-    geom_smooth(data=quotes, aes(x=date, y=sma_005, colour='sma_005'),
-        stat='identity') + 
-    geom_smooth(data=quotes, aes(x=date, y=sma_015, colour='sma_015'),
+    geom_line(data=quotes, aes(x=date, y=price), colour='black') + 
+    geom_smooth(data=quotes, aes(x=date, y=sma_015), colour='#661111',
         stat='identity') +
-    geom_smooth(data=quotes, aes(x=date, y=sma_045, colour='sma_045'),
+    geom_smooth(data=quotes, aes(x=date, y=sma_030), colour='#AA1111',
         stat='identity') +
-    geom_smooth(data=quotes, aes(x=date, y=sma_090, colour='sma_090'),
+    geom_smooth(data=quotes, aes(x=date, y=sma_060), colour='#FF1111',
         stat='identity') +
-    scale_colour_manual(
-        values=c('black', '#551111', '#881111', '#BB1111', '#FF1111'),
-        breaks=c('price', 'sma_005', 'sma_015', 'sma_045', 'sma_090')
-    ) +
-    labs(colour='Legend') +
+
+    geom_smooth(data=quotes, aes(x=date, y=price_growth),
+        colour='#111199', stat='identity') +
+    geom_smooth(data=quotes, aes(x=date, y=volume_growth_smooth),
+        colour='#119911', stat='identity') +
+    geom_smooth(data=quotes, aes(x=date, y=volume_growth),
+        colour='#119911', stat='identity') +
+    geom_hline(yintercept = ylimits[1] + growthSpan/2) +
+    scale_y_continuous(limit=ylimits, breaks=ylimits[1]:ylimits[2]) +
+    scale_x_date(breaks=quotes[c(1,(1:15)*6+1), 'date']) +
+    guides(colour=F) +
     xlab('date') +
     ylab('price');
 
--- lib/R/sma.R
+++ lib/R/sma.R
@@ -3,10 +3,11 @@
 sma = function(data, column, count) {
   seconds_per_day = 24*60*60;
   return(sapply(1:nrow(data), function(i) {
-    if (i < count) {
+    if (i < floor(count/2)) {
       return(NA);
     } else {
-      return(sum(data[(i-count+1):i, column]) / count);
+      start = i - floor(count/2);
+      return(sum(data[start:(start+count-1), column]) / count);
     }
   }));
 };
