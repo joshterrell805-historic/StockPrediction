05fa007c1defa55b3a8fbed5dd691db4aed71eab
Josh Terrell <joshterrell805@gmail.com>
2015-09-01 12:55:23 -0700

    Fix integer overlfow error in lib/R/sma

--- lib/R/sma.R
+++ lib/R/sma.R
@@ -1,13 +1,12 @@
 # calculate simple moving average of data
 # assumes data is at least 'count' long
 sma = function(data, column, count) {
-  seconds_per_day = 24*60*60;
   return(sapply(1:nrow(data), function(i) {
     if (i < floor(count/2)) {
       return(NA);
     } else {
       start = i - floor(count/2);
-      return(sum(data[start:(start+count-1), column]) / count);
+      return(sum(data[start:(start+count-1), column] / count));
     }
   }));
 };
