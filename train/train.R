004800bd9e1135f94533750c2cf99876ac0835a6
Josh Terrell <joshterrell805@gmail.com>
2015-09-02 02:53:57 -0700

    fix precision/recall measurements

--- train/train.R
+++ train/train.R
@@ -1,7 +1,8 @@
 source('lib/loadPreppedData.R');
 library(neuralnet);
 
-quotes_all = loadPreppedData();
+quotes_all = read.csv('data/AKS.prepped.csv');
+quotes_all = quotes_all[1:3000,];
 print('data loaded');
 
 
@@ -12,24 +13,27 @@ quotes_neg = quotes_all[quotes_all$should_buy == F,];
 quotes_neg = quotes_neg[sample(nrow(quotes_neg)),];
 
 
-train_size = 2000;
-quotes_train = quotes_neg[1:(train_size/2),];
-quotes_train = rbind(quotes_train, quotes_pos[1:(train_size/2),]);
+percentTrain = 0.40;
+trainPosEnd = floor(nrow(quotes_pos) * percentTrain);
+trainNegEnd = floor(nrow(quotes_neg) * percentTrain);
 
-test_size  = 2000;
-quotes_test = quotes_neg[(train_size+1):(train_size+1+test_size/2),];
-quotes_test = rbind(quotes_test,
-    quotes_pos[(train_size+1):(train_size+1+test_size/2),]);
+quotes_train = quotes_neg[1:trainNegEnd,];
+quotes_train = rbind(quotes_train, quotes_pos[1:trainPosEnd,]);
+
+quotes_test = quotes_neg[(trainNegEnd+1):nrow(quotes_neg),];
+quotes_test = rbind(quotes_test, quotes_pos[(trainPosEnd+1):nrow(quotes_pos),]);
 
 vars = colnames(quotes_all)[21:length(quotes_all)];
 print(vars);
 
 f = as.formula(paste('should_buy ~ ', paste(vars, collapse='+'))); 
-layers = c(length(vars), length(vars), length(vars), length(vars),
-    length(vars), length(vars), length(vars), length(vars), length(vars));
+# layers
+layers = c(length(vars), length(vars), length(vars), length(vars));#,
+#    length(vars), length(vars), length(vars), length(vars), length(vars));
 
 train_pos = nrow(quotes_train[quotes_train$should_buy == 1,]);
-print(paste('begin training; examples:', train_size, 'positives:', train_pos));
+print(paste('begin training; examples:', trainPosEnd+trainNegEnd, 'positives:',
+    trainPosEnd));
 net = neuralnet(f, quotes_train, hidden=layers, threshold=0.01);
 
 res = compute(net, quotes_test[,vars]);
@@ -38,19 +42,21 @@ res = cbind(quotes_test$date, quotes_test$should_buy,
 colnames(res) = c('date', 'should_buy', 'predicted');
 
 # calculate test error
-threshold = 0.9;
+threshold = 0.5;
 p = res[res$should_buy == T,];
 n = res[res$should_buy == F,];
 tp = nrow(p[p$predicted > threshold,]);
-fp = nrow(p[p$predicted > threshold,]);
-fn = nrow(n[n$predicted < threshold,]);
+fp = nrow(n[n$predicted > threshold,]);
+fn = nrow(p[p$predicted < threshold,]);
 tn = nrow(n[n$predicted < threshold,]);
-test_error = data.frame(a.t=c(tp, fp), a.f=c(fn, tn))
+test_error = data.frame(a.t=c(tp, fn), a.f=c(fp, tn))
 rownames(test_error) = c('p.t', 'p.f');
 
 # plot(net);
 print(res[1:10,]);
 print(paste('train error:', net$result.matrix[1,]));
 print(test_error);
-print(paste('precision:', tp / (tp + fp)));
-print(paste('recall:', tp / (tp + fn)));
+
+precision = tp / (tp + fp); recall = tp / (tp + fn);
+print(paste('precision:', precision));
+print(paste('recall:', recall));
