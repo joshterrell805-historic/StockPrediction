5f560ba256b577cdb41a011dd68d15fce84a111a
Josh Terrell <joshterrell805@gmail.com>
2015-09-01 19:17:40 -0700

    training adjustments

--- train/train.R
+++ train/train.R
@@ -12,26 +12,21 @@ quotes_neg = quotes_all[quotes_all$should_buy == F,];
 quotes_neg = quotes_neg[sample(nrow(quotes_neg)),];
 
 
-train_size = 300;
+train_size = 2000;
 quotes_train = quotes_neg[1:(train_size/2),];
 quotes_train = rbind(quotes_train, quotes_pos[1:(train_size/2),]);
 
-test_size  = 800;
-quotes_test = quotes_neg[1:(test_size/2),];
-quotes_test = rbind(quotes_test, quotes_pos[1:(test_size/2),]);
+test_size  = 2000;
+quotes_test = quotes_neg[(train_size+1):(train_size+1+test_size/2),];
+quotes_test = rbind(quotes_test,
+    quotes_pos[(train_size+1):(train_size+1+test_size/2),]);
 
-vars = c();
-for (d in c('sma', 'vma')) {
-  for (p in c(15, 30, 45)) {
-    vars = append(vars, paste(d, 'growth', '15', p, sep='_'));
-  }
-}
+vars = colnames(quotes_all)[21:length(quotes_all)];
 print(vars);
 
-
 f = as.formula(paste('should_buy ~ ', paste(vars, collapse='+'))); 
-layers = c(length(vars), length(vars)); #, length(vars), length(vars),
-#    length(vars), length(vars), length(vars), length(vars), length(vars));
+layers = c(length(vars), length(vars), length(vars), length(vars),
+    length(vars), length(vars), length(vars), length(vars), length(vars));
 
 train_pos = nrow(quotes_train[quotes_train$should_buy == 1,]);
 print(paste('begin training; examples:', train_size, 'positives:', train_pos));
@@ -43,11 +38,13 @@ res = cbind(quotes_test$date, quotes_test$should_buy,
 colnames(res) = c('date', 'should_buy', 'predicted');
 
 # calculate test error
-threshold = 0.5;
-tp = nrow(res[res$should_buy == 1 && res$predicted > threshold,]);
-fp = nrow(res[res$should_buy == 0 && res$predicted > threshold,]);
-fn = nrow(res[res$should_buy == 1 && res$predicted < threshold,]);
-tn = nrow(res[res$should_buy == 0 && res$predicted < threshold,]);
+threshold = 0.9;
+p = res[res$should_buy == T,];
+n = res[res$should_buy == F,];
+tp = nrow(p[p$predicted > threshold,]);
+fp = nrow(p[p$predicted > threshold,]);
+fn = nrow(n[n$predicted < threshold,]);
+tn = nrow(n[n$predicted < threshold,]);
 test_error = data.frame(a.t=c(tp, fp), a.f=c(fn, tn))
 rownames(test_error) = c('p.t', 'p.f');
 
