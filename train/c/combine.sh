eb55f4593dfa214499a55bad7043a0fa5aaab50f
Josh Terrell <joshterrell805@gmail.com>
2016-01-04 17:33:07 -0800

    not sure what this is
    I'm commiting commiting what was not committed.

--- train/c/combine.sh
+++ train/c/combine.sh
@@ -1,5 +1,5 @@
 function parseFannFileForExampleCount {
-  local firstLine=$(head -n 1 "data/$i.train.$2.fann")
+  local firstLine=$(head -n 1 "data/$i.$train_or_test.$2.fann")
   local numbers=
   IFS=' ' read -ra numbers <<< "$firstLine"
   local lines=${numbers[0]}
@@ -8,34 +8,44 @@ function parseFannFileForExampleCount {
   echo $lines
 }
 function parseFannFileForFeatureCount {
-  local firstLine=$(head -n 1 "data/$i.train.pos.fann")
+  local firstLine=$(head -n 1 "data/$i.$train_or_test.pos.fann")
   local numbers=
   IFS=' ' read -ra numbers <<< "$firstLine"
   local featureCount=${numbers[1]}
   echo $featureCount
 }
 function readFannFileExamples {
-  echo -n "$(tail -n +2 "data/$i.train.$2.fann")"
+  echo -n "$(tail -n +2 "data/$i.$train_or_test.$2.fann")"
 }
 
+train_or_test='train'
 IFS=' ' read -ra symbols <<< "$1"
-sum=0
-output=''
+sumPos=0
+sumNeg=0
+outputPos=''
+outputNeg=''
 for i in "${symbols[@]}"; do
-  sum=$(($sum + `parseFannFileForExampleCount $i 'pos'`));
-  sum=$(($sum + `parseFannFileForExampleCount $i 'neg'`));
+  sumPos=$(($sumPos + `parseFannFileForExampleCount $i 'pos'`));
+  sumNeg=$(($sumNeg + `parseFannFileForExampleCount $i 'neg'`));
 
   examples=`readFannFileExamples $i 'pos'`
-  output="$output
+  outputPos="$outputPos
 $examples"
   examples=`readFannFileExamples $i 'neg'`
-  output="$output
+  outputNeg="$outputNeg
 $examples"
 done
 
 featureCount=`parseFannFileForFeatureCount $1`
-output="$sum $featureCount 1
-$output"
-
 filename=$(echo "$1" | sed "s/ /_/g")
-echo -n "$output" > "data/$filename.train.fann"
+
+outputPos="$sumPos $featureCount 1 $outputPos"
+echo -n "$outputPos" > "data/$filename.$train_or_test.pos.fann"
+
+outputNeg=`echo "$outputNeg" | head -n $(($sumPos*2+1))`
+outputNeg="$sumPos $featureCount 1 $outputNeg"
+echo -n "$outputNeg" > "data/$filename.$train_or_test.neg.fann"
+
+echo "$((2*sumPos)) $featureCount 1
+`tail -n +2 "data/$filename.$train_or_test.neg.fann"`
+`tail -n +2 "data/$filename.$train_or_test.pos.fann"`" > "data/$filename.$train_or_test.fann"
